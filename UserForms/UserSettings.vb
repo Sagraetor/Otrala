004d3b45d9b24552859b9b49dff4dd9bf33c8a82
Irfan <irfan_21001145@utp.edu.my>
2022-06-27 17:42:26 +0800

    Added UserSettings

---
+++ UserForms/UserSettings.vb
@@ -0,0 +1,11 @@
+﻿Public Class UserSettings
+    ' log out 
+    Private Sub btnLogOut_Click(sender As Object, e As EventArgs) Handles btnLogOut.Click
+        Dim reply As MsgBoxResult = MsgBox("Thank you for using Otrala" + Environment.NewLine +
+                                           "See you when we 'Travel Lagi'", MsgBoxStyle.OkCancel, "Exit")
+        If reply = MsgBoxResult.Ok Then
+            Me.Close()
+        End If
+    End Sub
+
+End Class
