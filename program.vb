48c2d816a38cf7e8785998d5d9dd16a9d888a960
Sia <sia_21001570@utp.edu.my>
2022-07-10 23:32:18 +0800

    added sign in to admin username:  admin pass: admin

--- program.vb
+++ program.vb
@@ -2,6 +2,6 @@
     Shared Sub Main()
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(True)
-        Application.Run(Admin)
+        Application.Run(Catalogue)
     End Sub
 End Class
